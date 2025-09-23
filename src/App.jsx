94a4591181162f76631c3ef8dee7b8634f3eec32
n3rz2008 <daniel.silva.roch2@gmail.com>
2025-09-23 20:43:37 -0300

    cards content and icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Menu from './components/Menu'
 import Footer from './components/Footer'
 import Card from "./components/Card";
 import { Title, Paragraph, Container, Image, Button } from './components/pageComponents'
+import { SmileIcon, VideoIcon, WalkIcon } from './components/icons';
 import "./components/styles/cards.css"
 
 function App() {
@@ -42,10 +43,15 @@ function App() {
         <Container style={{
           padding: "25px", flex: 1
         }}>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
+          <Card icon={<SmileIcon/>}>
+            Aprenda a entender e gerenciar suas emoções, a se comunicar com mais clareza e a construir relacionamentos mais saudáveis. Nossas ferramentas e atividades são criadas para te ajudar a desenvolver habilidades como empatia, resiliência e autoconhecimento, que são fundamentais tanto na vida pessoal quanto profissional. 
+          </Card>
+          <Card icon={<VideoIcon/>}>
+            Navegue por uma vasta biblioteca de conteúdos criados por especialistas. Nossos cursos e aulas abordam temas como direitos celetistas, inteligência emocional e comunicação não violenta, de forma prática e acessível. Você terá acesso a informações valiosas para aplicar no seu dia a dia e se sentir mais preparado para os desafios do mercado de trabalho. 
+          </Card>
+          <Card icon={<WalkIcon/>}>
+            Sabemos que cada jornada é única. Por isso, oferecemos a possibilidade de atendimento com profissionais dedicados para te guiar e tirar suas dúvidas de forma individual. Conte com o apoio de quem entende do assunto para traçar um caminho de desenvolvimento personalizado e focado nas suas necessidades. 
+          </Card>
         </Container>
       </div>
       <Footer />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
 import "./styles/cards.css"
 
-export default function Card({children}) {
+export default function Card({icon, children}) {
     return <div className="card">
+        <div className="cardImg">
+            {icon}
+        </div>
         {children}
     </div>
 }
